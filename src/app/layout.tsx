667f168b43e9b6f86d72b64d55c9a0bb800dd504
Adarsh Mamgain <52105807+adarsh-mamgain@users.noreply.github.com>
2024-07-13 16:30:08 +0530

    🚀 v1.6 - added og and twitter metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,20 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Create T3 App",
-  description: "Created with ❤️ by @adarsh-mamgain",
+  title: "Conway's Game of Life",
+  description: "Created with ❤️ by https://x.com@adarsh_mamgain",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  authors: [{ name: "Adarsh Mamgain", url: "https://x.com/adarsh_mamgain" }],
+  openGraph: {
+    title: "Conway's Game of Life",
+    description: "Created with ❤️ by https://x.com@adarsh_mamgain",
+    images: [{ url: "/assets/meta-image.webp" }],
+  },
+  twitter: {
+    creatorId: "@adarsh_mamgain",
+    description: "Created with ❤️ by https://x.com@adarsh_mamgain",
+    images: "/assets/meta-image.webp",
+  },
 };
 
 export default function RootLayout({
